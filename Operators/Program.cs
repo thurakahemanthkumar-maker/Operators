681bf5984bd1b35b18c8f78a6ef867207215610e
Hemanth Kumar <thurakahemanthkumar@gmail.com>
2026-06-22 17:45:28 +0530

    Add project files.

---
+++ Operators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    /* Git Hub: repository where we have all our code.
+* Push the code to repository using Visual Studio.
+
+* Operator:
+* Operator is used to operate between two operands.
+* Ex: c = a + b;
+* Here a and b are called operands where "+" is an operator.
+* Types of Operators:
+* 1) Arithmetic Operator
+* 2) Assignment Operator
+* 3) Increment and Decrement Operator
+* 4) Relational Operator
+* 5) Bitwise Operator
+* 6) Conditional Operator*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
